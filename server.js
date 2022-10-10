b93d6457f13d69f22e748731fe05b863a95bc3ba
kevocode22 <ocampokevin14@gmail.com>
2022-10-09 21:13:08 -0300

    Controllers for CRUD cellphones

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const dotenv = require('dotenv');
 const express = require('express');
 const app = express();
-
+const Router = require('./routes/routes');
 
 require('dotenv').config()
 require('./config/database')
 
+const port = process.env.PORT;
 
 dotenv.config();
 app.get('/', (req, res) => {
   res.send('Server is running');
 });
-
-const port = process.env.PORT;
+app.use(express.json())
+app.use('/api', Router)
 app.listen(port, () => {
-  console.log(`[server]: Server is running at https://localhost:${port}`);
+  console.log(`[server]: Server is running at http://localhost:${port}`);
 });
