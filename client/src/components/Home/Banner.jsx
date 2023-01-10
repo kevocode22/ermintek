7a8d9cb18979585b5ec2aab5efe95fca043977a0
kevocode22 <ocampokevin14@gmail.com>
2023-01-10 16:35:37 -0300

    changed text Banner

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -4,12 +4,12 @@ import './home.css'
 const Banner = () => {
 
   return (
-    <div className="videobanner flex justify-start items-center w-full my-7 -z-50">
+    <div className="videobanner flex justify-start items-center w-full my-7 -z-50 sm:max-h-32">
 <video autoPlay muted loop className="w-full h-screen object-cover -z-10">
 <source src='https://i.imgur.com/dlRhXlM.mp4' className="-z-30"/> 
 </video>
 <div className="flex flex-col items-center" >
-  <h2 className="textBanner">Nuevo Iphone 14 Pro</h2>
+  <h2 className="textBanner sm:text-md">Nuevo Iphone 14 Pro</h2>
 </div>
 </div>
   ) 
