b8607226198310cffd90ed696850b4565ee43426
kevocode22 <ocampokevin14@gmail.com>
2023-01-12 00:29:46 -0300

    disabled video controllers fron banner

--- client/src/components/Home/Banner.jsx
+++ client/src/components/Home/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
 
   return (
     <div className="videobanner flex justify-start items-center w-full my-7 -z-50">
-<video autoPlay muted loop className="w-full h-screen object-cover -z-10">
+<video autoPlay muted loop controls="false" className="w-full h-screen object-cover -z-10">
 <source src='https://i.imgur.com/dlRhXlM.mp4' className="-z-30"/> 
 </video>
 <div className="flex flex-col items-center">
